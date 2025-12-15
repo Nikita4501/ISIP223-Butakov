9d63479cf9be6bf68cd42f0b49ea861f5ecc6f2e
234928 <234928@KB37-118-C5>
2025-12-15 10:32:49 +0300

    Добавлен класс BossFactory

---
+++ ISIP223-Butakov/Model/Factories/BossFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model.Factories
+{
+    public static class BossFactory
+    {
+        public static Boss CreateBoss()
+        {
+            int bossType = RandomChoice.GetRandomNumber(4);
+
+            switch (bossType)
+            {
+                case 0:
+                    return new VVG();
+                case 1:
+                    return new Kovalsky();
+                case 2:
+                    return new ArchmageCPP();
+                case 3:
+                    return new PestovC();
+                default:
+                    return new VVG();
+            }
+        }
+    }
+}
