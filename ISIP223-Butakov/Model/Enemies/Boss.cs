e1e80069024fcda7af2cf99be0e319c82b16a4b4
234928 <234928@KB37-118-C5>
2025-12-15 10:34:49 +0300

    Добавлен класс Boss

---
+++ ISIP223-Butakov/Model/Enemies/Boss.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model.Enemies
+{
+    public abstract class Boss : Enemy
+    {
+        // Базовые характеристики для расчета множителей
+        protected const int BASE_HP = 30;
+        protected const int BASE_ATTACK = 12;
+        protected const int BASE_DEFENSE = 3;
+    }
+}
