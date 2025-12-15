6a5a26c44c5ca6ca13e0ce83d6801b23b459eb41
234928 <234928@KB37-118-C5>
2025-12-15 10:30:18 +0300

    Добавлен класс EnemyFactory

---
+++ ISIP223-Butakov/Model/Factories/EnemyFactory.cs
@@ -0,0 +1,31 @@
+﻿using ISIP223_Butakov.Model.Enemies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model.Factories
+{
+    public static class EnemyFactory
+    {
+        public static Enemy CreateEnemy()
+        {
+            int enemyType = RandomChoice.GetRandomNumber(4); // Теперь 4 типа монстров
+
+            switch (enemyType)
+            {
+                case 0:
+                    return new Goblin();
+                case 1:
+                    return new Skeleton();
+                case 2:
+                    return new Mage();
+                case 3:
+                    return new Slime();
+                default:
+                    return new Goblin();
+            }
+        }
+    }
+}
