6f84c0cdc88b935e884c243538c6d61c4ddfa712
234928 <234928@KB37-118-C5>
2025-12-15 10:22:13 +0300

    Добавлены классы Weapon и Armor

---
+++ ISIP223-Butakov/Model/Armor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model
+{
+    public class Armor
+    {
+        public int Defense { get; }
+
+        public Armor(int defense)
+        {
+            Defense = defense;
+        }
+    }
+}
---
+++ ISIP223-Butakov/Model/Weapon.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model
+{
+    public class Weapon
+    {
+        public int Attack { get; }
+
+        public Weapon(int attack)
+        {
+            Attack = attack;
+        }
+    }
+}
