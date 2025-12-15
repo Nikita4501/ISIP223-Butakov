d07c7668f80dc9cf66a38d90dcf12b86ff962a12
234928 <234928@KB37-118-C5>
2025-12-15 10:15:08 +0300

    Добавлен класс RandomChoice

---
+++ ISIP223-Butakov/Model/RandomChoice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model
+{
+    public static class RandomChoice
+    {
+        private static Random random = new Random();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            return random.Next(min, max);
+        }
+
+        public static int GetRandomNumber(int max)
+        {
+            return random.Next(max);
+        }
+
+        public static bool GetChance(int percentage)
+        {
+            return random.Next(100) < percentage;
+        }
+
+        public static double GetRandomDouble()
+        {
+            return random.NextDouble();
+        }
+    }
+}
