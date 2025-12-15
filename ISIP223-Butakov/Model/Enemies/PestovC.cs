db97426f68daf43b3fcd5848f9762e2ea421e400
234928 <234928@KB37-118-C5>
2025-12-15 10:38:28 +0300

    Добавлен класс PestovC

---
+++ ISIP223-Butakov/Model/Enemies/PestovC.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model.Enemies
+{
+    public class PestovC : Boss
+    {
+        public PestovC()
+        {
+            Name = "Пестов С--";
+            HP = (int)(BASE_HP * 1.3);
+            Attack = (int)(BASE_ATTACK * 1.8);
+            Defense = (int)(BASE_DEFENSE * 0.6);
+        }
+
+        public override void SpecialAttack(Player player, int blockedDamage, ref bool playerFrozen)
+        {
+            // Игнорирует защиту игрока + шанс заморозки
+            int damage = Attack;
+
+            // 40% шанс заморозки (25% базовый + 15%)
+            if (RandomChoice.GetChance(40))
+            {
+                Console.WriteLine($"Пестов С-- атакует с заморозкой, игнорируя защиту! Нанесено {damage} урона, вы пропустите следующий ход!");
+                playerFrozen = true;
+                player.TakeDamage(damage);
+            }
+            else
+            {
+                Console.WriteLine($"Пестов С-- атакует, игнорируя защиту! Нанесено {damage} урона!");
+                player.TakeDamage(damage);
+            }
+        }
+    }
+}
