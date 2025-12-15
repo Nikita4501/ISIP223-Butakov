f6a77248158b97e16dae7d9d6ad6087fce4e9221
234928 <234928@KB37-118-C5>
2025-12-15 10:24:58 +0300

    Добавлен класс Enemy

---
+++ ISIP223-Butakov/Model/Enemies/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP223_Butakov.Model.Enemies
+{
+    public abstract class Enemy
+    {
+        public string Name { get; protected set; }
+        public int HP { get; protected set; }
+        public int Attack { get; protected set; }
+        public int Defense { get; protected set; }
+        public bool IsAlive => HP > 0;
+
+        public void TakeDamage(int damage)
+        {
+            HP -= damage;
+            if (HP < 0) HP = 0;
+        }
+
+        public abstract void SpecialAttack(Player player, int blockedDamage, ref bool playerFrozen);
+    }
+}
